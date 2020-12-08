1c9b21579eea4a25ee0fb7c61fb8e9b5adefaf87
Glassy1982 <sydrther@gmail.com>
2020-12-08 09:22:23 +0000

    successful database connection with admin

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Page
 
 
 # Create your views here
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Page
     template_name = 'home.html'
 
 
